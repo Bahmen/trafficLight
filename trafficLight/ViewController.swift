6b9a26368576bace6f309a9863c3f9353018e0a0
Bahmen <4181035@gmail.com>
2023-03-18 23:36:25 +0600

    temp

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightArea: UIView!
+    
+    @IBOutlet weak var yellowLightArea: UIView!
+    
+    @IBOutlet weak var greenLightArea: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redLightArea.alpha = 0.3
+        redLightArea.layer.cornerRadius = 50
+        
+        yellowLightArea.alpha = 0.3
+        yellowLightArea.layer.cornerRadius = 50
+        
+        greenLightArea.alpha = 0.3
+        greenLightArea.layer.cornerRadius = 50
     }
 
-
+    
+    @IBAction func startButtonDidTapped(_ sender: UIButton) {
+        redLightArea.alpha = 1
+        sender.setTitle("NEXT", for: .normal)
+    }
 }
 
